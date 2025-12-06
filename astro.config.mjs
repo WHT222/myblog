f2ef76155789d670c505e51061fc5aa8d234bb70
王海涛 <3230103360@zju.edu.cn>
2025-12-06 20:47:08 +0800

    Update site and base URL in astro.config.mjs
    初步尝试

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,8 +26,8 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://fuwari.vercel.app/",
-	base: "/",
+	site: "https://WHT222.github.io",
+	base: "/myblog",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
