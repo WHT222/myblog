6bba638652a19056ba2bbc6fc2ac5b6888ca8feb
王海涛 <3230103360@zju.edu.cn>
2025-12-07 18:47:19 +0800

    修改了标题和语言

--- src/config.ts
+++ src/config.ts
@@ -8,11 +8,12 @@ import type {
 import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
-	title: "Fuwari",
-	subtitle: "Demo Site",
-	lang: "en", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
+	title: "芙芙的小蛋糕",
+	subtitle: "记录生活与学习",
+	lang: "zh_CN", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
 	themeColor: {
 		hue: 250, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
+				//默认色调，从0到360。例如红色：0，青色：200，青色：250，粉色：345
 		fixed: false, // Hide the theme color picker for visitors
 	},
 	banner: {
